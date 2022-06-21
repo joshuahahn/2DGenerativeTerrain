59e693f4cd6d16184c5e3b368cab147d496b477a
joshuahahn <joshua.hahnjy@gmail.com>
2022-06-21 11:52:41 -0400

    Finished File reading

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
-import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Initial states
+        final int N = 4;
+        final int M = 3;
+        List<Integer> AllStates = new ArrayList<>(List.of(0,1,2,3,4));
+        
+
         ArrayList<Ruleset> Rules = null; // Adjacency rules
         Scanner in = null;
         File file = null;
@@ -15,41 +23,69 @@ public class Main {
             in = new Scanner(file);
             Rules = readRules(in);
             in.close();
+            System.out.println("Ruleset Input Complete");
         } 
         catch (Exception e){
             System.out.println("File input error");
             e.printStackTrace();
         }
+
+        // Create NxM array that stores possible states of each tile
+        // Also store entropy of ecah tile
+        List<List<Set<Integer>>> States = new ArrayList<List<Set<Integer>>>(N);
+
+        // Initialize all cells in States with all possible states.
+        for (int i = 0; i < N; i++) {
+            List<Set<Integer>> columns = new ArrayList<>(M);
+            for (int j = 0; j < M; j++) {
+                columns.add(new HashSet<>(AllStates));
+            }
+            States.add(columns);
+        }
+
+        // TODO: Initial state modification
+
+        // Create NxM array that stores entropy of each tile
+        int[][] Entropy = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                Entropy[i][j] = States.get(i).get(j).size();
+                System.out.println(Entropy[i][j]);
+            }
+        }
+
+        
     }
 
     public static ArrayList<Ruleset> readRules(Scanner file) {
         int N = file.nextInt(); // Number of modules
-        String temp = file.next();
         ArrayList<Ruleset> rules = new ArrayList<>();
         
         for (int i = 0; i < N; i++) {
             Ruleset ruleset = new Ruleset(i);
             // Data cleanup
-            String token = file.nextLine();
+            String token = file.next();
             String[] tokens = token.split(",");
             int idx = 0;
 
-            while (tokens[idx] != "*") {
+            while (!tokens[idx].equals("*")) {
                 ruleset.setNorth(Integer.parseInt(tokens[idx]));
                 idx++;
             }
             idx++;
-            while (tokens[idx] != "*") {
+            while (!tokens[idx].equals("*")) {
                 ruleset.setEast(Integer.parseInt(tokens[idx]));
+                idx++;
             }
             idx++;
-            while (tokens[idx] != "*") {
+            while (!tokens[idx].equals("*")) {
                 ruleset.setSouth(Integer.parseInt(tokens[idx]));
                 idx++;
             }
             idx++;
-            while (tokens[idx] != "*") {
+            while (!tokens[idx].equals("*")) {
                 ruleset.setWest(Integer.parseInt(tokens[idx]));
+                idx++;
             }
             rules.add(ruleset);
         }
--- Ruleset.java
+++ Ruleset.java
@@ -1,5 +1,6 @@
 import java.util.HashSet;
 import java.util.ArrayList;
+import java.lang.StringBuilder;
 
 public class Ruleset {
     int index;
@@ -26,5 +27,30 @@ public class Ruleset {
     }
     public void setWest(Integer i) {
         this.rules.get(3).add(i);
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("North: ");
+        for (Integer i : rules.get(0)) {
+            sb.append(i);
+            sb.append(", ");
+        }
+        sb.append("East: ");
+        for (Integer i : rules.get(1)) {
+            sb.append(i);
+            sb.append(", ");
+        }
+        sb.append("South: ");
+        for (Integer i : rules.get(2)) {
+            sb.append(i);
+            sb.append(", ");
+        }
+        sb.append("West: ");
+        for (Integer i : rules.get(3)) {
+            sb.append(i);
+            sb.append(", ");
+        }
+        return sb.toString();
     }
 }
